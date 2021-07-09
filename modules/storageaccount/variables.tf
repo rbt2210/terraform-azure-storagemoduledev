3245945fe9f29aae0cfcbb8f326deb08cb1b3223
rbt2210 <42622041+rbt2210@users.noreply.github.com>
2021-07-09 12:37:43 +0530

    added new module

---
+++ modules/storageaccount/variables.tf
@@ -0,0 +1,13 @@
+variable "storageaccount" {
+  type = string
+  default = "rahulstr"
+}
+
+# To define madnatory input value
+variable "containername" {
+  type = string
+}
+
+variable "resourcegroup" {
+  type =  string
+}
